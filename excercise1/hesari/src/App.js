efb2140b6ede15b25b7dde514780709e00db1906
Niibi <you@example.com>
2022-01-28 16:14:47 +0200

    mmm

--- excercise1/hesari/src/App.js
+++ excercise1/hesari/src/App.js
@@ -2,10 +2,12 @@
 import './App.css';
 import Maincontent from './Components/Maincontent';
 import Header from './Components/Header';
+import Adbanner from './Components/Adbanner';
 function App() {
   return (
     <div className="App">
       <Header />
+      <Adbanner />
      <Maincontent />
      
     </div>
--- excercise1/hesari/src/Components/Maincontent.js
+++ excercise1/hesari/src/Components/Maincontent.js
@@ -1,7 +1,23 @@
 import React from 'react';
 
 export default function Maincontent() {
-  return <div> 
-      <h1>LOL</h1>
+  return <div className="maincontent">
+        
+        
+        <div className="mainPage">
+          <div className="mainHeadline">Kuva
+          <div className="morso">
+            <img className="lol" src="https://hs.mediadelivery.fi/img/468/dfd36d621f214fd4afaf03f28f606f7f.jpg"></img>
+            </div>
+            
+            </div>
+
+            <div className="sideBar">
+
+
+            </div>
+        </div>
+
+     
   </div>;
 }
