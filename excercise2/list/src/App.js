8663be6734055c855f1e747d04841434aa954547
Niibix <t0nani00@students.oamk.fi>
2022-02-10 14:53:37 +0200

    3

--- excercise2/list/src/App.js
+++ excercise2/list/src/App.js
@@ -2,7 +2,7 @@ import Pohjanpaska from './components/Pohjanpaska';
 import Shit from './components/Shit';
 import Buttons from './components/Buttons';
 import './App.css';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 
 
 
@@ -10,37 +10,46 @@ import { useState, useEffect } from 'react';
 
 function App() {
 
-/*
-  const[randomNumber, setRandomNumber] = useState(0);
-  */
+
 
   const [benneItems ,setBenneItems] = useState([
     { 
+
       id:1,
       qty: 0,
       name: "Karhu"
+
     },
     { 
+
       id:2,
       qty: 0,
       name: "Koskenkorva"
+
     },
     { 
+
       id:3,
       qty: 0,
       name: "Piri"
+
     },
     { 
+
       id:4,
       qty: 0,
       name: "Heroiini"
+      
     }
+
   ]);
 
+
+
+
 const onKarhuAddition = (idCheck) =>{
+
 var randomNumber = Math.floor(Math.random() *10) +1;
-
-
 
 let newbenneItems = [...benneItems];
 
@@ -61,12 +70,7 @@ if(itemClickedIdex != -1) {
 
   return (
     <div className="App">
-
-
-     
     <Pohjanpaska />
-
-
     <Shit Shit={benneItems} />
     <Buttons Kossu= { onKarhuAddition } />
 
